ea45dcca865a4d940c484ecf3b3ff0d7bb679101
kh1msh0 <giorgikhimshiashvili@gmail.com>
2019-10-07 12:41:09 +0400

    aaa

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,17 @@ const Footer = () => {
   return (
     <div>
       <div className="footer_">
-      <div className="socials">
-            <a href="https://www.facebook.com/Areage-190230005186117/">
-              <FaFacebookF />
-            </a>
-            <a href="https://www.instagram.com/area.ge/">
-              <FaInstagram />
-            </a>
-            <a href="https://www.linkedin.com/company/area-ge/">
-              <FaLinkedinIn />
-            </a>
-          </div>
+        <div className="socials">
+          <a href="https://www.facebook.com/Areage-190230005186117/">
+            <FaFacebookF />
+          </a>
+          <a href="https://www.instagram.com/area.ge/">
+            <FaInstagram />
+          </a>
+          <a href="https://www.linkedin.com/company/area-ge/">
+            <FaLinkedinIn />
+          </a>
+        </div>
         <div className="content">
           <div className="container">
             <div className="colums location loc">
@@ -23,42 +23,66 @@ const Footer = () => {
               <div className="location_colums">
                 <div className="colum">
                   <div>
-                    <a href="https://area.ge/search?address_cityId=1">თბილისი</a>
+                    <a href="https://area.ge/search?address_cityId=1">
+                      თბილისი
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/search?address_cityId=93">ქუთაისი</a>
+                    <a href="https://area.ge/search?address_cityId=93">
+                      ქუთაისი
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/search?address_cityId=109">გუდაური</a>
+                    <a href="https://area.ge/search?address_cityId=109">
+                      გუდაური
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/search?address_cityId=100">აბაშა</a>
+                    <a href="https://area.ge/search?address_cityId=100">
+                      აბაშა
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/search?address_cityId=102">ადიგენი</a>
+                    <a href="https://area.ge/search?address_cityId=102">
+                      ადიგენი
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/search?address_cityId=104">ანაკლია</a>
+                    <a href="https://area.ge/search?address_cityId=104">
+                      ანაკლია
+                    </a>
                   </div>
                 </div>
                 <div className="colum">
                   <div>
-                    <a href="https://area.ge/search?address_cityId=92">ბათუმი</a>
+                    <a href="https://area.ge/search?address_cityId=92">
+                      ბათუმი
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/search?address_cityId=78">ბაკურიანი</a>
+                    <a href="https://area.ge/search?address_cityId=78">
+                      ბაკურიანი
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/search?address_cityId=106">აბასთუმანი</a>
+                    <a href="https://area.ge/search?address_cityId=106">
+                      აბასთუმანი
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/search?address_cityId=101">აგარა</a>
+                    <a href="https://area.ge/search?address_cityId=101">
+                      აგარა
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/search?address_cityId=103">ამბროლაური</a>
+                    <a href="https://area.ge/search?address_cityId=103">
+                      ამბროლაური
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/search?address_cityId=105">ანანური</a>
+                    <a href="https://area.ge/search?address_cityId=105">
+                      ანანური
+                    </a>
                   </div>
                 </div>
               </div>
@@ -83,16 +107,17 @@ const Footer = () => {
                     <a href="https://area.ge/search?type=16">ოფისი</a>
                   </div>
                 </div>
-
-              
               </div>
             </div>
             <div className="colums location">
-              <div className="footer_headers">აღმოაჩინე <span style={{fontFamily:'NotoSans_bold'}}>AREA</span></div>
+              <div className="footer_headers">
+                აღმოაჩინე{" "}
+                <span style={{ fontFamily: "NotoSans_bold" }}>AREA</span>
+              </div>
               <div className="location_colums">
                 <div className="colum">
                   <div>
-                    <a href="https://area.ge/ka/about">ჩვენს შესახებ</a>
+                    <a href="https://area.ge/ka/about">ჩვენ შესახებ</a>
                   </div>
                   <div>
                     <a href="http://blog.area.ge">ბლოგი</a>
@@ -101,13 +126,16 @@ const Footer = () => {
                     <a href="https://area.ge/ka/faq">ხშირად დასმული კითხვები</a>
                   </div>
                   <div>
-                    <a href="https://static.area.ge/docs/terms-and-conditions-ka.pdf">გამოყენების პირობები</a>
+                    <a href="https://static.area.ge/docs/terms-and-conditions-ka.pdf">
+                      გამოყენების პირობები
+                    </a>
                   </div>
                   <div>
-                    <a href="https://static.area.ge/docs/privacy-policy-ka.pdf">კონფიდენციალობის წესები</a>
+                    <a href="https://static.area.ge/docs/privacy-policy-ka.pdf">
+                      კონფიდენციალობის წესები
+                    </a>
                   </div>
                 </div>
-
               </div>
             </div>
             <div className="colums location">
@@ -115,40 +143,62 @@ const Footer = () => {
               <div className="location_colums">
                 <div className="colum">
                   <div>
-                    <a href="https://area.ge/ka/premium-services/make-featured">აქციე შეთავაზებად</a>
+                    <a href="https://area.ge/ka/premium-services/make-featured">
+                      აქციე შეთავაზებად
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/ka/premium-services/photographer">ფოტოგრაფი</a>
+                    <a href="https://area.ge/ka/premium-services/photographer">
+                      ფოტოგრაფი
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/ka/premium-services/content-editor">განცხადების რედაქტორი</a>
+                    <a href="https://area.ge/ka/premium-services/content-editor">
+                      განცხადების რედაქტორი
+                    </a>
                   </div>
                   <div>
-                    <a href="https://area.ge/ka/premium-services/advertise">რეკლამა სოციალურ მედიაში</a>
+                    <a href="https://area.ge/ka/premium-services/advertise">
+                      რეკლამა სოციალურ მედიაში
+                    </a>
                   </div>
                 </div>
-
-              
               </div>
             </div>
             <div className="colums location">
               <div className="footer_headers">დაგვიკავშირდით</div>
               <div className="location_colums">
-                <div className="contact" style={{justifyContent:'center'}}>
-                  <div style={{marginLeft:'1px'}}>
-                    <a href="0#"><img style={{ width:'14px'}} src={require('../images/phone.png')}/></a>
+                <div className="contact" style={{ justifyContent: "center" }}>
+                  <div style={{ marginLeft: "1px" }}>
+                    <a href="0#">
+                      <img
+                        style={{ width: "12px" }}
+                        src={require("../images/phone.png")}
+                      />
+                    </a>
                   </div>
                   <div>
-                    <a href="0#"><img style={{ width:'16px'}} src={require('../images/email.png')}/></a>
+                    <a href="0#">
+                      <img
+                        style={{ width: "16px" }}
+                        src={require("../images/email.png")}
+                      />
+                    </a>
                   </div>
                   <div>
-                    <a href="https://www.facebook.com/Areage-190230005186117/"><FaFacebookF /> </a>
+                    <a href="https://www.facebook.com/Areage-190230005186117/">
+                      <FaFacebookF />{" "}
+                    </a>
                   </div>
                   <div>
-                    <a href="https://www.instagram.com/area.ge/"><FaInstagram /></a>
+                    <a href="https://www.instagram.com/area.ge/">
+                      <FaInstagram />
+                    </a>
                   </div>
                   <div>
-                    <a href="https://www.linkedin.com/company/area-ge/"><FaLinkedinIn /></a>
+                    <a href="https://www.linkedin.com/company/area-ge/">
+                      <FaLinkedinIn />
+                    </a>
                   </div>
                 </div>
 
@@ -169,7 +219,6 @@ const Footer = () => {
                     <a href="">Linkedin</a>
                   </div>
                 </div>
-
               </div>
             </div>
           </div>
